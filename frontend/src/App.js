536c707056d9b392c6b99ea261e5a2ce685b10a9
Red1O <mocadarius69@gmail.com>
2024-10-03 21:58:09 +0300

    a2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-       {/* <Route
-          path="/"
-          element={<Home />}        
-        />*/}
+        <Home/>
       </Routes>
     </div>
     </BrowserRouter>

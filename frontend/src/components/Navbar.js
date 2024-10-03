305f4b3064713d7f38177020a4e3dd04f1ff067d
Red1O <mocadarius69@gmail.com>
2024-10-03 21:42:43 +0300

    a1

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+
+const Navbar=() => {
+  return(
+    <div>
+        <div className="wrapper">
+      <nav className="nav">
+        <div>
+          <a href="/" className="logo">InterSTORY</a>
+        </div>
+        <div className="nav-items">
+          <a href="/">Search by keyword</a>
+          <a className="menu"href="/">Menu</a>
+        </div>
+      </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,53 @@
+import qin from "../asssets/qin.jpeg"
+
+const Home =() => {
+  return(
+<section className="hero">
+            <div className="hero-container">
+                <div className="welcome">
+                    <h1>Welcome to InterSTORY</h1>
+                    <p>Choose your story:</p>
+                </div>
+            </div>
+            <div className="container">
+                <div className="cell">
+                    <div>
+                        <img src={qin} className="photo" alt="Dinastia Qin"/>
+                    </div>
+                    <div className="photo-explanation">
+                        <h2>Dinastia Qin</h2>
+                        <p>Crearea si caderea dinastiei Qin 221-206 I.H.</p>
+                    </div>
+                </div>
+                <div className="cell">
+                  <div>
+                    <img src={qin} className="photo" alt="Dinastia Qin"/> 
+                  </div>
+                  <div className="photo-explanation">
+                      <h2>Dinastia Qin</h2>
+                      <p>Crearea si caderea dinastiei Qin 221-206 I.H.</p>
+                  </div>
+              </div>
+              <div className="cell">
+                <div>
+                  <img src={qin} className="photo" alt="Dinastia Qin"/>
+                </div>
+                <div className="photo-explanation">
+                    <h2>Dinastia Qin</h2>
+                    <p>Crearea si caderea dinastiei Qin 221-206 I.H.</p>
+                </div>
+            </div>
+              <div className="cell">
+                <div>
+                  <img src={qin} className="photo" alt="Dinastia Qin"/>
+                </div>
+                <div className="photo-explanation">
+                    <h2>Dinastia Qin</h2>
+                    <p>Crearea si caderea dinastiei Qin 221-206 I.H.</p>
+                </div>
+            </div>
+          </div>
+        </section>
+  );
+}
+export default Home
